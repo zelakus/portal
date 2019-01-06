77797ab13ec0d7258cb123caeb7363b841df17a4
Kizi Tekuto <paradoxht@hotmail.com>
2019-01-06 23:54:08 +0300

    local config files

---
+++ backend/config/main-local.php
@@ -0,0 +1,31 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'wJhpyGuGBcfxCpH-uF_lK-Vao7GkYVNq',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['*'],
+        'generators' => [
+            'kouoslModule' => [
+                'class' => 'kouosl\gii\module\Generator'
+            ]
+        ]
+    ];
+}
+
+return $config;
